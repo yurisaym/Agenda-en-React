8855fa83a935e96a686968ad9bf6c75fb1d61343
yurisaym <yurisaynunes13@gmail.com>
2022-12-04 23:45:35 -0300

    primer commit inicializando el proyecto de agenda

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  FormGroup,
+  ModalFooter
+} from 'reactstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  { id: 1, personaje: 'Danilson', anime: 'vicio' },
+  { id: 2, personaje: 'Yurisay', anime: 'lala' },
+  { id: 3, personaje: 'Summer', anime: 'mascota' },
+  { id: 4, personaje: 'Pepe', anime: 'sexual' }
+]
+
+class App extends React.Component {
+  state = {
+    data: data
+  }
+
+  render() {
+    return (
+      <>
+        <Container>
+          <br />
+          <Button color="success">Insertar nuevo personaje</Button>
+          <br />
+          <br />
+
+          <Table>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Personaje</th>
+                <th>Anime</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.data.map(elemento => (
+                <tr>
+                  <td>{elemento.id}</td>
+                  <td>{elemento.personaje}</td>
+                  <td>{elemento.anime}</td>
+                  <td>
+                    <Button color="primary">Editar</Button>
+                    {'  '}
+                    <Button color="danger">Eliminar</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
